b7e396f9808698f736521e86d5cd78672021ddfb
zoubaidadriss2@gmail.com <zoubaida.driss@yahoo.com>
2022-10-30 14:13:01 +0100

    my first react project on github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import Form from 'react-bootstrap/Form'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button} from 'react-bootstrap';
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: 'block', 
+                  width: 700, 
+                  padding: 30 }}>
+      <h4>checkpoint form</h4>
+      <Form>
+      <Form.Group>
+          <Form.Label>Enter your full name:</Form.Label>
+          <Form.Control type="text" 
+                        placeholder="Enter your full name" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Enter your email address:</Form.Label>
+          <Form.Control type="email" 
+                        placeholder="Enter your your email address" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Enter your age:</Form.Label>
+          <Form.Control type="number" placeholder="Enter your age" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          submit
+        </Button>
+      </Form>
     </div>
   );
 }
-
-export default App;
